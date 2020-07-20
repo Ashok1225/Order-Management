bc7aea8171521c4e7b760f0bd414043cf8657346
Ashok1225 <31406175+Ashok1225@users.noreply.github.com>
2020-07-20 13:10:17 +0530

    Update Order.sql

--- Order.sql
+++ Order.sql
@@ -1,1 +1,6 @@
-
+CREATE  Order IF NOT EXISTS `H2`.`Order` (
+  `CustomerNmae` INT NOT NULL ,
+  `OrderDate` VARCHAR(45) NULL ,
+  `ShippingAddress` VARCHAR(45) NULL,
+  'OrderItems' VARCHAR(45) NULL,
+  PRIMARY KEY (`CustomerName`));
